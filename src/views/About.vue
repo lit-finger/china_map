d6354ddedf944b9677f404cfab4b6bf9bfd24ef7
unknown <yan118.li@tcl.com>
2021-09-16 17:28:56 +0800

    地图文件引入

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="chart"></div>
   </div>
 </template>
+
+<script>
+import china from "../assets/china"
+
+export default {
+  data(){
+    return{
+      option:{}
+    }
+  },
+  mounted(){
+    this.$echarts.registerMap('china', china);
+  }
+}
+</script>
